8f5c737712d74291ac6e1cdf41dd753658f5fd19
Melike Işık <melike.34.37@hotmail.com>
2021-02-01 01:56:14 +0300

    Add project files.

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //interface operasyonları default publictir.
+    public interface IProductDal  //Dal=Data Access Layer
+    {//Veritabanında yapacağımız operasyonları içeren Interface
+        List<Product> GetAll();
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+
+        List<Product> GetAllByCategory(int categoryId); //ürünleri kategoriye göre filtrele 
+    
+    }
+}
---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Product:IEntity //public yaptığımız zaman bu classa diğer katmanlar da ulaşabilir. (Business(ürünü kontrol edecek), DataAccess(ürünü ekleyecek),ConsoleUI(ürünü gösterecek))
+    { //bir classın defaultu internal'dir. Sadece Entities erişebilir.
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public short UnitsInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+
+
+    }
+}
